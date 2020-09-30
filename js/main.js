72528769b6eb733cb9e4675e7e785e1b6ab52665
genmiyazato <sapporogentaro@gmail.com>
2020-09-30 14:57:15 +0900

    main.js scroll add

--- js/main.js
+++ js/main.js
@@ -2,35 +2,40 @@
 let height = $(".navbar").height();
 
 //ヘッダーの高さ分だけコンテンツを下げる
-$(function () {
-  $("body").css("margin-top", height); //10pxだけ余裕をもたせる
+jQuery(function () {
+    jQuery("body").css("margin-top", height); //10pxだけ余裕をもたせる
 });
 
 // ナビゲーションリンクをクリックしたら、スクロールする
-$(function () {
-  $(".nav-link").click(function () {
-    // headerの高さを指定
-    let headerHeight = height + 10;
-    // animateの速さを指定
-    let speed = 1000;
-    // クリックされた要素のhref属性を取得
-    let href = $(this).attr("href");
-    // href属性が#かつ空白であれば、'html',そうでなければ、変数hrefを格納
-    let target = $(href == "#" || href == "" ? "html" : href);
-    // ヘッダーの高さを引いたtargetの座標を取得
-    let position = target.offset().top - headerHeight;
+jQuery(function () {
 
-    // html,bodyをアニメーション
-    $("html, body").animate(
-      {
-        scrollTop: position,
-      },
-      speed,
-      "swing"
-    );
+    jQuery('a[href^="https://musica.local/#"').click(function () {
+        // headerの高さを指定
+        let headerHeight = height + 10;
+        // animateの速さを指定
+        let speed = 1000;
+        // クリックされた要素のhref属性を取得
+        let href = jQuery(this).attr("href");
 
-    // ハンバーガーメニューを閉じる
-    $(".collapse").collapse("hide");
-    return false;
-  });
+        // hrefのurl https://musica.local/を削除
+        const deleteHttps = 'https://musica.local/';
+        let scrollTarget = href.replace(deleteHttps, '');
+
+        // href属性が#かつ空白であれば、'html',そうでなければ、変数hrefを格納
+        let target = jQuery(href == "#" || href == "" ? "html" : scrollTarget);
+        // ヘッダーの高さを引いたtargetの座標を取得
+        let position = target.offset().top - headerHeight;
+
+        // html,bodyをアニメーション
+        jQuery("html, body").animate({
+                scrollTop: position,
+            },
+            speed,
+            "swing"
+        );
+
+        // ハンバーガーメニューを閉じる
+        jQuery(".collapse").collapse("hide");
+        return false;
+    });
 });
